58ad391a817b3e42cd366253b6df331ba9e493bb
XReid <xmeister86@gmail.com>
2019-01-19 10:44:39 -0800

    removed temp file

--- src/server/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const timestamp = () => {
-            const date = Date(Date.now).toString();
-            let dateArr = date.split(' ');
-            return dateArr.slice(0, 5).join(' ');
-        };
-        console.log(timestamp());
